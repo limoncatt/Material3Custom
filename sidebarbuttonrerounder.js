6a370b096a9486392c043ef9732ae8d974684903
limoncatt <75332327+limoncatt@users.noreply.github.com>
2026-02-08 01:19:24 +0100

    upd

--- sidebarbuttonrerounder.js
+++ sidebarbuttonrerounder.js
@@ -6,3 +6,30 @@ document.addEventListener('DOMContentLoaded', () => {
             children[children.length - 1].style.borderRadius = "8px 8px 28px 28px";
     }
 });
+
+const homebutton = document.querySelector(".homebutton")
+
+homebutton.addEventListener('click', () => {
+    showOnly('homepage');
+})
+
+const projectsbutton = document.querySelector(".projectsbutton")
+
+projectsbutton.addEventListener('click', () => {
+    showOnly('projectspage');
+})
+
+
+const showOnly = (className) => {
+    const children = document.querySelectorAll('.contentpage > *');
+
+    children.forEach(child => {
+        if (child.classList.contains(className)) {
+            child.style.display = '';
+        } else {
+            child.style.display = 'none';
+        }
+    });
+};
+
+showOnly('homepage');
